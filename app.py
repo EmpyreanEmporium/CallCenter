3bf47cc25b79dc5b10bab8b6ec634b81ab2ec82f
Empyrean Dev <Matthew.Flack.1980@outlook.com>
2024-01-17 08:39:24 -0500

    1.17.24 Morning Update

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import os
 import sqlite3
 import csv
 
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
-from helpers import usd
+
+from helpers import usd, login_required
 
 #configure app
 app = Flask(__name__)
@@ -21,7 +23,21 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = sqlite3("sqlite:///sqlite3/callcenter.db")
+db = SQL("sqlite:///callcenter.db")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    #clear any user_ids
+    session.clear()
+
+    # POST
+    if not request.form.get("username"):
+        return render_template("login.html")
 
 
+@app.route("/", method=["GET", "POST"])
+@login_required
+def index():
+    return render_template("login.html")
 
--- helpers.py
+++ helpers.py
@@ -1,4 +1,16 @@
 # setup
+from functools import wraps
+from flask import redirect, render_template, session
+
+
+def login_required(f):
+    
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 def usd(value):
     """Format value as USD."""
