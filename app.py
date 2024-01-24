de4ed5062510832c1d2cf1f2ebc94e718f1dd17a
Empyrean Dev <Matthew.Flack.1980@outlook.com>
2024-01-24 00:11:57 -0500

    1.24.24 Update

--- app.py
+++ app.py
@@ -23,21 +23,75 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///callcenter.db")
+db = SQL("sqlite:///sqlite3/callcenter.db")
+
+# Cache 
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    # Log out by clearing session
+    session.clear()
+
+    # Return to index
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    #clear any possible other sessions
+    session.clear()
+
+    # Variable for avalability check for username
+    exist = db.execute("SELECT * FROM employees WHERE username = ?", request.form.get("username"))
+
+    if request.method == "GET":
+        return render_template
+    
+    if request.method == "POST":
+        # Check for empty field
+        if not request.form.get("username"):
+            return render_template("register.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
-    #clear any user_ids
+    # Clear any user_ids
     session.clear()
 
-    # POST
-    if not request.form.get("username"):
-        return render_template("login.html")
+    # Check forms for content
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return render_template("login.html")
+        
+        elif not request.form.get("password"):
+            return render_template("login.html")
+        
+        # Query database for username
+        rows = db.execute("SELECT * FROM employees WHERE username = ?", request.form.get("username"))
 
+        # Make sure username exist and passward is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("login.html")
+        
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
 
-@app.route("/", method=["GET", "POST"])
-@login_required
-def index():
-    return render_template("login.html")
+        #redirect for sucessful login
+        return render_template("app.html")
+         
+
 
